45f6b1e9d49335ba2a6750412231f09cd3c2def3
Nat Evatt <natevatt@gmail.com>
2017-10-01 09:13:47 -0700

    saving new styles to db

--- src/actions/mapActions.js
+++ src/actions/mapActions.js
@@ -2,34 +2,39 @@ import * as types from './actionTypes';
 import mapStylesApi from '../api/mapStylesApi';
 
 export function loadMapStylesSuccess(mapStyles){
-  return { type: types.LOAD_MAPSTYLES_SUCCESS, mapStyles};
+    return { type: types.LOAD_MAPSTYLES_SUCCESS, mapStyles};
 }
 
 export function trackMapMovementSuccess(mapMovements){
-  return { type: types.TRACK_MAP_MOVE_SUCCESS, mapMovements};
+    return { type: types.TRACK_MAP_MOVE_SUCCESS, mapMovements};
 }
 
 export function saveNewStyleSuccess(newStyle){
-  return { type: types.SAVE_NEW_STYLE_SUCCESS, newStyle};
+    return { type: types.SAVE_NEW_STYLE_SUCCESS, newStyle};
 }
 
 export function loadMapStyles(){
-  return function(dispatch) {
-    return mapStylesApi.getMapStyles().then(mapStyles => {
-      dispatch(loadMapStylesSuccess(mapStyles));
-    }).catch(error => {
-      throw(error);
-    });
-  };
+    return function(dispatch) {
+        return mapStylesApi.getMapStyles().then(mapStyles => {
+            dispatch(loadMapStylesSuccess(mapStyles));
+        }).catch(error => {
+            throw(error);
+        });
+    };
 }
 
 export function trackMapMovement(mapMovements){
-  return function(dispatch) {
-      dispatch(trackMapMovementSuccess(mapMovements));
-  };
+    return function(dispatch) {
+        dispatch(trackMapMovementSuccess(mapMovements));
+    };
 }
 export function saveNewStyle(newStyle){
-  return function(dispatch) {
-      dispatch(saveNewStyleSuccess(newStyle));
-  };
+    return function(dispatch) {
+        return mapStylesApi.saveNewMapStyle(newStyle).then(id => {
+            newStyle['id'] = id;
+            dispatch(saveNewStyleSuccess(newStyle));
+        }).catch(error => {
+            throw(error);
+        });
+    };
 }
--- src/api/mapStylesApi.js
+++ src/api/mapStylesApi.js
@@ -3,11 +3,21 @@ import Congfig from '../config';
 
 class MapStylesApi {
     static getMapStyles() {
-          return fetch(Congfig.apiRoot + 'vts-api/public/get_mapstyles')
-          .then((response) => response.json())
-          .then((responseJson) => {
+        return fetch(Congfig.apiRoot + 'vts-api/public/get_mapstyles')
+        .then((response) => response.json())
+        .then((responseJson) => {
             return responseJson;
         });
+    }
+
+    static saveNewMapStyle(newStyle) {
+        return fetch(Congfig.apiRoot + 'vts-api/public/save_new_map_style', {
+            method: 'POST',
+            body: JSON.stringify(newStyle)
+        }).then((response) => response.json())
+        .then((responseJson) => {
+            return responseJson
+        })
     }
 }
 
